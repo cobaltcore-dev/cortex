3657445d2e152bcbac4a1eb45840620d120547fd
Philipp Matthes <p.matthes@sap.com>
2025-03-14 14:34:56 +0100

    Add tests

---
+++ internal/mqtt/mqtt_test.go
@@ -0,0 +1,81 @@
+// Copyright 2025 SAP SE
+// SPDX-License-Identifier: Apache-2.0
+
+package mqtt
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cobaltcore-dev/cortex/internal/conf"
+	"github.com/cobaltcore-dev/cortex/testlib/db/containers"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func TestConnect(t *testing.T) {
+	if os.Getenv("VERNEMQ_CONTAINER") != "1" {
+		t.Skip("skipping test; set VERNEMQ_CONTAINER=1 to run")
+	}
+
+	container := containers.VernemqContainer{}
+	container.Init(t)
+	defer container.Close()
+	conf := conf.MQTTConfig{URL: "tcp://localhost:" + container.GetPort()}
+	client := NewClient(conf)
+
+	err := client.Connect()
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
+
+func TestPublish(t *testing.T) {
+	if os.Getenv("VERNEMQ_CONTAINER") != "1" {
+		t.Skip("skipping test; set VERNEMQ_CONTAINER=1 to run")
+	}
+
+	container := containers.VernemqContainer{}
+	container.Init(t)
+	defer container.Close()
+	conf := conf.MQTTConfig{URL: "tcp://localhost:" + container.GetPort()}
+	client := NewClient(conf)
+
+	err := client.Publish("test/topic", map[string]string{"key": "value"})
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
+
+func TestSubscribe(t *testing.T) {
+	if os.Getenv("VERNEMQ_CONTAINER") != "1" {
+		t.Skip("skipping test; set VERNEMQ_CONTAINER=1 to run")
+	}
+
+	container := containers.VernemqContainer{}
+	container.Init(t)
+	defer container.Close()
+	conf := conf.MQTTConfig{URL: "tcp://localhost:" + container.GetPort()}
+	client := NewClient(conf)
+
+	err := client.Subscribe("test/topic", func(client mqtt.Client, msg mqtt.Message) {})
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
+
+func TestDisconnect(t *testing.T) {
+	if os.Getenv("VERNEMQ_CONTAINER") != "1" {
+		t.Skip("skipping test; set VERNEMQ_CONTAINER=1 to run")
+	}
+
+	container := containers.VernemqContainer{}
+	container.Init(t)
+	defer container.Close()
+	conf := conf.MQTTConfig{URL: "tcp://localhost:" + container.GetPort()}
+	client := NewClient(conf)
+	err := client.Connect()
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+	client.Disconnect()
+}
