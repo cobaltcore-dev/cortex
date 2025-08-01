35d3b4a365f3833db9bbc07aa66dedc01e7474ac
Philipp Matthes <p.matthes@sap.com>
2025-08-01 08:19:16 +0200

    Add status field to limes commitment

--- internal/sync/openstack/limes/limes_types_test.go
+++ internal/sync/openstack/limes/limes_types_test.go
@@ -7,14 +7,6 @@ import (
 	"testing"
 )
 
-func TestCommitment_TableName(t *testing.T) {
-	commitment := Commitment{}
-	expected := "openstack_limes_commitments"
-	if commitment.TableName() != expected {
-		t.Errorf("expected %s, got %s", expected, commitment.TableName())
-	}
-}
-
 func TestCommitment_Indexes(t *testing.T) {
 	commitment := Commitment{}
 	indexes := commitment.Indexes()
