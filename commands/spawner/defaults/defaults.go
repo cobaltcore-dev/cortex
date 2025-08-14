829459bb7ae9345e2664c3ca9e081a25ef99b161
Philipp Matthes <p.matthes@sap.com>
2025-08-14 09:51:15 +0200

    Move workload spawner here [skip ci]

---
+++ commands/spawner/defaults/defaults.go
@@ -0,0 +1,53 @@
+// Copyright 2025 SAP SE
+// SPDX-License-Identifier: Apache-2.0
+
+package defaults
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/sapcc/go-bits/must"
+)
+
+type Defaults interface {
+	// GetDefault returns the default value for the given key.
+	GetDefault(key string) string
+	// SetDefault sets the default value for the given key.
+	SetDefault(key string, value string)
+}
+
+type defaults struct {
+	// Where to store the defaults.
+	filename string
+}
+
+func NewDefaults(filename string) Defaults {
+	return &defaults{filename: filename}
+}
+
+func (d *defaults) GetDefault(key string) string {
+	file, err := os.Open(d.filename)
+	if err != nil {
+		return ""
+	}
+	defer file.Close()
+	var defaults map[string]string
+	must.Succeed(json.NewDecoder(file).Decode(&defaults))
+	return defaults[key]
+}
+
+func (d *defaults) SetDefault(key, value string) {
+	var defaults map[string]string
+	file, err := os.Open(d.filename)
+	if err != nil {
+		defaults = make(map[string]string)
+	} else {
+		defer file.Close()
+		must.Succeed(json.NewDecoder(file).Decode(&defaults))
+	}
+	defaults[key] = value
+	newFile := must.Return(os.Create(".defaults.json"))
+	defer newFile.Close()
+	must.Succeed(json.NewEncoder(newFile).Encode(defaults))
+}
