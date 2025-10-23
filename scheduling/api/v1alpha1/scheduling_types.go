905ca46ad8f71263c48d4f1542c941d345ef29e1
Philipp Matthes <p.matthes@sap.com>
2025-10-23 10:46:37 +0200

    More api spec templating

--- scheduling/api/v1alpha1/scheduling_types.go
+++ scheduling/api/v1alpha1/scheduling_types.go
@@ -4,18 +4,155 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+// The type of scheduling.
+type SchedulingType string
+
+const (
+	// The scheduling was created by the nova external scheduler call.
+	// Usually we refer to this as nova initial placement, it also includes
+	// migrations or resizes.
+	SchedulingTypeNova SchedulingType = "nova"
+	// The scheduling was created by the cinder external scheduler call.
+	SchedulingTypeCinder SchedulingType = "cinder"
+	// The scheduling was created by the manila external scheduler call.
+	SchedulingTypeManila SchedulingType = "manila"
+	// The scheduling was created by spawning an ironcore machine.
+	SchedulingTypeMachine SchedulingType = "machine"
 )
 
 type SchedulingSpec struct {
 	// The operator by which this scheduling should be extracted.
 	Operator string `json:"operator,omitempty"`
+
+	// When there is a source host for the scheduling, it is recorded here.
+	//
+	// Note: for initial placements, this will be empty. However, for migrations
+	// or resizes, this will contain the source host.
+	// +kubebuilder:validation:Optional
+	SourceHost string `json:"sourceHost,omitempty"`
+
+	// A reference to the pipeline that should be used for this scheduling.
+	// This reference can be used to look up the pipeline definition and its
+	// scheduler step configuration for additional context.
+	PipelineRef corev1.ObjectReference `json:"pipelineRef"`
+
+	// An identifier for the underlying resource to be scheduled.
+	// For example, this can be the UUID of a nova instance or cinder volume.
+	// This can be used to correlate multiple schedulings for the same resource.
+	ResourceID string `json:"resourceID"`
+
+	// The type of scheduling, indicating what has initiated this scheduling.
+	Type SchedulingType `json:"type"`
+	// If the type is "nova", this field contains the raw nova scheduling request.
+	// +kubebuilder:validation:Optional
+	NovaRaw *runtime.RawExtension `json:"novaRaw,omitempty"`
+	// If the type is "cinder", this field contains the raw cinder scheduling request.
+	// +kubebuilder:validation:Optional
+	CinderRaw *runtime.RawExtension `json:"cinderRaw,omitempty"`
+	// If the type is "manila", this field contains the raw manila scheduling request.
+	// +kubebuilder:validation:Optional
+	ManilaRaw *runtime.RawExtension `json:"manilaRaw,omitempty"`
+	// If the type is "machine", this field contains the machine reference.
+	// +kubebuilder:validation:Optional
+	MachineRef *corev1.ObjectReference `json:"machineRef,omitempty"`
+}
+
+type SchedulingDecisionType string
+
+const (
+	// Scheduling decision for the nova external scheduler call.
+	SchedulingDecisionTypeNova SchedulingDecisionType = "nova"
+	// Scheduling decision for the cinder external scheduler call.
+	SchedulingDecisionTypeCinder SchedulingDecisionType = "cinder"
+	// Scheduling decision for the manila external scheduler call.
+	SchedulingDecisionTypeManila SchedulingDecisionType = "manila"
+	// Scheduling decision for an ironcore machine.
+	SchedulingDecisionTypeMachine SchedulingDecisionType = "machine"
+)
+
+type NovaSchedulingDecision struct {
+	// Sorted list of compute hosts from more preferred to least preferred.
+	ComputeHosts []string `json:"computeHosts"`
+	// Outputs of the scheduling pipeline including the activations used
+	// to make the final ordering of compute hosts.
+	Activations map[string]float64 `json:"activations,omitempty"`
+}
+
+type CinderSchedulingDecision struct {
+	// Sorted list of storage hosts from more preferred to least preferred.
+	StoragePools []string `json:"storagePools"`
+	// Outputs of the scheduling pipeline including the activations used
+	// to make the final ordering of storage hosts.
+	Activations map[string]float64 `json:"activations,omitempty"`
+}
+
+type ManilaSchedulingDecision struct {
+	// Sorted list of share hosts from more preferred to least preferred.
+	StoragePools []string `json:"storagePools"`
+	// Outputs of the scheduling pipeline including the activations used
+	// to make the final ordering of share hosts.
+	Activations map[string]float64 `json:"activations,omitempty"`
+}
+
+type MachineSchedulingDecision struct {
+	// Sorted list of machine pools from more preferred to least preferred.
+	MachinePools []string `json:"machinePools"`
+	// Outputs of the scheduling pipeline including the activations used
+	// to make the final ordering of machine pools.
+	Activations map[string]float64 `json:"activations,omitempty"`
 }
 
 type SchedulingStatus struct {
 	// The time it took to schedule.
 	// +kubebuilder:validation:Optional
 	Took metav1.Duration `json:"took"`
+
+	// When there is a designated target host for the scheduling, it is recorded
+	// here.
+	//
+	// Note: for external scheduler requests, this will be the first host from
+	// the list of returned hosts -- meaning there is no guarantee this is
+	// actually the host where the resource will be spawned on. Please check
+	// the decision details to see the full list of hosts and their scores.
+	//
+	// For deschedulings, this will be empty, indicating there is no specific
+	// target host.
+	// +kubebuilder:validation:Optional
+	TargetHost string `json:"targetHost,omitempty"`
+
+	// The type of scheduling decision made.
+	// +kubebuilder:validation:Optional
+	DecisionType SchedulingDecisionType `json:"decisionType,omitempty"`
+	// If the scheduling decision type is "nova", this field contains the
+	// nova scheduling decision.
+	// +kubebuilder:validation:Optional
+	NovaDecision *NovaSchedulingDecision `json:"novaDecision,omitempty"`
+	// If the scheduling decision type is "cinder", this field contains the
+	// cinder scheduling decision.
+	// +kubebuilder:validation:Optional
+	CinderDecision *CinderSchedulingDecision `json:"cinderDecision,omitempty"`
+	// If the scheduling decision type is "manila", this field contains the
+	// manila scheduling decision.
+	// +kubebuilder:validation:Optional
+	ManilaDecision *ManilaSchedulingDecision `json:"manilaDecision,omitempty"`
+	// If the scheduling decision type is "machine", this field contains the
+	// machine scheduling decision.
+	// +kubebuilder:validation:Optional
+	MachineDecision *MachineSchedulingDecision `json:"machineDecision,omitempty"`
+
+	// If there were previous schedulings for the underlying resource, they will
+	// be resolved here to provide historical context for the scheduling.
+	// +kubebuilder:validation:Optional
+	History []corev1.ObjectReference `json:"history,omitempty"`
+
+	// A human-readable explanation of the scheduling result.
+	// +kubebuilder:validation:Optional
+	Explanation string `json:"explanation,omitempty"`
 
 	// If there was an error during the last scheduling, it is recorded here.
 	// +kubebuilder:validation:Optional
--- scheduling/go.mod
+++ scheduling/go.mod
@@ -9,8 +9,16 @@ replace (
 )
 
 require (
+	github.com/cobaltcore-dev/cortex/decisions/api v0.0.0-20251023063201-2a0f914492eb
+	github.com/cobaltcore-dev/cortex/knowledge/api v0.0.0-20251023063201-2a0f914492eb
 	github.com/cobaltcore-dev/cortex/lib v0.0.0-00010101000000-000000000000
+	github.com/cobaltcore-dev/cortex/reservations/api v0.0.0-20251023063201-2a0f914492eb
 	github.com/cobaltcore-dev/cortex/scheduling/api v0.0.0-00010101000000-000000000000
+	github.com/cobaltcore-dev/cortex/testlib v0.0.0-00010101000000-000000000000
+	github.com/gophercloud/gophercloud/v2 v2.8.0
+	github.com/majewsky/gg v1.3.0
+	github.com/sapcc/go-api-declarations v1.17.4
+	github.com/sapcc/go-bits v0.0.0-20251016133802-c2d049e762e6
 	k8s.io/apimachinery v0.34.1
 	k8s.io/client-go v0.34.1
 	sigs.k8s.io/controller-runtime v0.22.3
@@ -92,14 +100,36 @@ require (
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/continuity v0.4.5 // indirect
+	github.com/dlmiddlecote/sqlstats v1.0.2 // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.5.1 // indirect
+	github.com/go-gorp/gorp v2.2.0+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.19.0 // indirect
+	github.com/gorilla/websocket v1.5.4-0.20250319132907-e064f32e3674 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	github.com/moby/sys/user v0.4.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/opencontainers/runc v1.3.0 // indirect
+	github.com/ory/dockertest v3.3.5+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.23.2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.67.1 // indirect
 	github.com/prometheus/procfs v0.17.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/net v0.46.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
