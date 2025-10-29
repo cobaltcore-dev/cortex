ad333d1314cef9c1ae08e567fabd89dd552baf7e
Philipp <27271818+PhilippMatthes@users.noreply.github.com>
2025-10-29 15:28:15 +0100

    Remove outdated trigger files (#361)

--- knowledge/api/datasources/openstack/cinder/triggers.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2025 SAP SE
-// SPDX-License-Identifier: Apache-2.0
-
-package cinder
-
-// Trigger executed when new storage pools are available.
-const TriggerCinderStoragePoolsSynced = "triggers/sync/openstack/cinder/types/storage_pools"
