44b446997dafa562a622731993a6989408822d76
Philipp Matthes <p.matthes@sap.com>
2025-10-29 14:30:26 +0100

    Remove outdated trigger files

--- knowledge/api/datasources/openstack/limes/triggers.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2025 SAP SE
-// SPDX-License-Identifier: Apache-2.0
-
-package limes
-
-// Trigger executed when new resource providers are available.
-const TriggerLimesCommitmentsSynced = "triggers/sync/openstack/limes/types/commitments"
