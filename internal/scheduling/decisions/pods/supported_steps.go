f65803a520dd1cefb694c092dca55acf32c10adc
Valentin Küchler <42544122+Varsius@users.noreply.github.com>
2026-01-19 10:26:59 +0100

    Affinity taint toleration filters (#456)
    This PR refactors the directory structure for filters/weighers of the
    pod pipeline to align with the nova and manila pipeline. Secondly,
    filters are added that implement the default Kubernetes behavior for
    hard taints, tolerations and affinities. Soft requirements regarding
    these behaviors, which would need to be implemented as weighers, are not
    included in this PR and are subject for future work.

--- internal/scheduling/decisions/pods/supported_steps.go
+++ internal/scheduling/decisions/pods/supported_steps.go
@@ -5,6 +5,7 @@ package pods
 
 import (
 	"github.com/cobaltcore-dev/cortex/api/delegation/pods"
+	"github.com/cobaltcore-dev/cortex/internal/scheduling/decisions/pods/plugins/filters"
 	"github.com/cobaltcore-dev/cortex/internal/scheduling/lib"
 )
 
@@ -13,5 +14,7 @@ type PodStep = lib.Step[pods.PodPipelineRequest]
 // Configuration of steps supported by the scheduling.
 // The steps actually used by the scheduler are defined through the configuration file.
 var supportedSteps = map[string]func() PodStep{
-	"noop": func() PodStep { return &NoopFilter{} },
+	"noop":         func() PodStep { return &filters.NoopFilter{} },
+	"taint":        func() PodStep { return &filters.TaintFilter{} },
+	"nodeaffinity": func() PodStep { return &filters.NodeAffinityFilter{} },
 }
