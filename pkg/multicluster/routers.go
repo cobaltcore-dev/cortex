360a491358c25f41e61b0e3520d6cb14250d4908
Markus Wieland <markus.wieland@sap.com>
2026-03-19 10:02:33 +0100

    Add ReservationsResourceRouter for routing reservations based on availability zone

--- pkg/multicluster/routers.go
+++ pkg/multicluster/routers.go
@@ -6,6 +6,7 @@ package multicluster
 import (
 	"errors"
 
+	"github.com/cobaltcore-dev/cortex/api/v1alpha1"
 	hv1 "github.com/cobaltcore-dev/openstack-hypervisor-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -38,6 +39,30 @@ func (h HypervisorResourceRouter) Match(obj any, labels map[string]string) (bool
 		return false, errors.New("hypervisor does not have availability zone label")
 	}
 	return hvAZ == az, nil
+}
+
+// ReservationsResourceRouter routes reservations to clusters based on availability zone.
+type ReservationsResourceRouter struct{}
+
+func (r ReservationsResourceRouter) Match(obj any, labels map[string]string) (bool, error) {
+	var res v1alpha1.Reservation
+	switch v := obj.(type) {
+	case *v1alpha1.Reservation:
+		res = *v
+	case v1alpha1.Reservation:
+		res = v
+	default:
+		return false, errors.New("object is not a Reservation")
+	}
+	az, ok := labels["az"]
+	if !ok {
+		return false, errors.New("cluster does not have availability zone label")
+	}
+	resAZ := res.Spec.AvailabilityZone
+	if resAZ == "" {
+		return false, errors.New("reservation does not have availability zone label")
+	}
+	return resAZ == az, nil
 }
 
 // TODO: Add router for Decision CRD and reservations after their refactoring is done.
