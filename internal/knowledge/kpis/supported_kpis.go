58de2cb76156ee63146c72f0d58f07332d580b6c
Markus Wieland <markus.wieland@sap.com>
2026-05-06 12:14:10 +0200

    refactor: remove deprecated infrastructure kpis

--- internal/knowledge/kpis/supported_kpis.go
+++ internal/knowledge/kpis/supported_kpis.go
@@ -16,8 +16,6 @@ var supportedKPIs = map[string]plugins.KPI{
 	"kvm_host_capacity_kpi":        &compute.KVMResourceCapacityKPI{},
 	"vmware_host_contention_kpi":   &compute.VMwareHostContentionKPI{},
 	"vmware_project_noisiness_kpi": &compute.VMwareProjectNoisinessKPI{},
-	"host_running_vms_kpi":         &compute.HostRunningVMsKPI{},
-	"flavor_running_vms_kpi":       &compute.FlavorRunningVMsKPI{},
 	"vm_migration_statistics_kpi":  &compute.VMMigrationStatisticsKPI{},
 	"vm_life_span_kpi":             &compute.VMLifeSpanKPI{},
 	"vm_commitments_kpi":           &compute.VMCommitmentsKPI{},
