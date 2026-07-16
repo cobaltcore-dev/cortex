8f869844878808fcc61cf24d67c54a89b93ee48d
Markus Wieland <markus.wieland@sap.com>
2026-07-16 10:23:15 +0200

    fix: remove unused claude artifact
    Signed-off-by: Markus Wieland <markus.wieland@sap.com>

--- pkg/multicluster/testing/client.go
+++
@@ -1,6 +0,0 @@
-// Copyright SAP SE
-// SPDX-License-Identifier: Apache-2.0
-
-// Package multiclustertest provides helpers for building *multicluster.Client
-// instances in unit tests without a real Kubernetes API server.
-package multiclustertest
